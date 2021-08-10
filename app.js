7d5c85108da9295e108a1444d8bfac4c381f39d9
Jordy Vargas <jordyvargas182@yahoo.com>
2021-08-10 12:09:01 -0600

    :construction: Agregue slider de marcas - Jordy Vargas

--- app.js
+++ app.js
@@ -12,3 +12,16 @@ menu.addEventListener('click', function(){
 window.addEventListener('scroll', function(){
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+
+//ANIMACION DEL SLIDER
+ onload = start;
+
+ function start(){
+     var i = 1;
+     function Move(){
+         i = (i%6)+1 //Cambiar el numero 4 por la cantidad real de imagenes en el slider
+         document.getElementById('i'+i).checked = true;
+     }
+     setInterval(Move,3000) //3000 = 3s
+ }
