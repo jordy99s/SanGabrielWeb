aa78932cfd2cf90a915079c248720e34bc736a03
Jordy Vargas <jordyvargas182@yahoo.com>
2021-08-09 23:24:12 -0600

    :construction: Agregue responsividad a footer - Jordy Vargas

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar-menu')
 
+const header = document.querySelector('.navbar');
+
 menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active')
+    menuLinks.classList.toggle('active');
 })
+
+//This fixes the navigation bar at the top of the page
+window.addEventListener('scroll', function(){
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
